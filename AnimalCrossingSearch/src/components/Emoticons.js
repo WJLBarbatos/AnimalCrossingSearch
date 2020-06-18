c7b8f62aca7ee29562619975013ee0ed80a09c51
wujl100 <wujl100@chinaunicom.cn>
2020-06-18 16:12:21 +0800

    plan to add search emotiicons

---
+++ AnimalCrossingSearch/src/components/Emoticons.js
@@ -0,0 +1,311 @@
+export const emoticons = [
+    {
+        id: "01",  //ID
+        name: "点头",  //名称
+        icon: "",  //icon
+        type: "暴躁",  //类型
+        sp: 0,
+    },
+    {
+        id: "02",  //ID
+        name: "嗯？",  //名称
+        icon: "",  //icon
+        type: "暴躁",  //类型
+        sp: 0,
+    },
+    {
+        id: "03",  //ID
+        name: "苦笑",  //名称
+        icon: "",  //icon
+        type: "暴躁",  //类型
+        sp: 0,
+    },
+    {
+        id: "04",  //ID
+        name: "烦恼",  //名称
+        icon: "",  //icon
+        type: "暴躁",  //类型
+        sp: 0,
+    },
+    {
+        id: "05",  //ID
+        name: "灵光一闪",  //名称
+        icon: "",  //icon
+        type: "暴躁",  //类型
+        sp: 2,
+    },
+    {
+        id: "06",  //ID
+        name: "紧张",  //名称
+        icon: "",  //icon
+        type: "运动",  //类型
+        sp: 0,
+    },
+    {
+        id: "07",  //ID
+        name: "加油",  //名称
+        icon: "",  //icon
+        type: "运动",  //类型
+        sp: 0,
+    },
+    {
+        id: "08",  //ID
+        name: "吓一跳！",  //名称
+        icon: "",  //icon
+        type: "运动",  //类型
+        sp: 0,
+    },
+    {
+        id: "09",  //ID
+        name: "阿嚏",  //名称
+        icon: "",  //icon
+        type: "运动",  //类型
+        sp: 0,
+    },
+    {
+        id: "10",  //ID
+        name: "舞动",  //名称
+        icon: "",  //icon
+        type: "运动",  //类型
+        sp: 2,
+    },
+    {
+        id: "11",  //ID
+        name: "拍手",  //名称
+        icon: "",  //icon
+        type: "悠闲",  //类型
+        sp: 1,
+    },
+    {
+        id: "12",  //ID
+        name: "害羞",  //名称
+        icon: "",  //icon
+        type: "悠闲",  //类型
+        sp: 0,
+    },
+    {
+        id: "13",  //ID
+        name: "糟糕",  //名称
+        icon: "",  //icon
+        type: "悠闲",  //类型
+        sp: 0,
+    },
+    {
+        id: "14",  //ID
+        name: "不好意思",  //名称
+        icon: "",  //icon
+        type: "悠闲",  //类型
+        sp: 0,
+    },
+    {
+        id: "15",  //ID
+        name: "大哭",  //名称
+        icon: "",  //icon
+        type: "悠闲",  //类型
+        sp: 0,
+    },
+    {
+        id: "16",  //ID
+        name: "得意",  //名称
+        icon: "",  //icon
+        type: "悠闲",  //类型
+        sp: 2,
+    },
+    {
+        id: "17",  //ID
+        name: "你好！",  //名称
+        icon: "",  //icon
+        type: "自恋",  //类型
+        sp: 1,
+    },
+    {
+        id: "18",  //ID
+        name: "睡着了",  //名称
+        icon: "",  //icon
+        type: "自恋",  //类型
+        sp: 0,
+    },
+    {
+        id: "19",  //ID
+        name: "心碎",  //名称
+        icon: "",  //icon
+        type: "自恋",  //类型
+        sp: 0,
+    },
+    {
+        id: "20",  //ID
+        name: "好了好了",  //名称
+        icon: "",  //icon
+        type: "自恋",  //类型
+        sp: 0,
+    },
+    {
+        id: "21",  //ID
+        name: "微笑",  //名称
+        icon: "",  //icon
+        type: "自恋",  //类型
+        sp: 0,
+    },
+    {
+        id: "22",  //ID
+        name: "看我的",  //名称
+        icon: "",  //icon
+        type: "自恋",  //类型
+        sp: 2,
+    },
+    {
+        id: "23",  //ID
+        name: "开心",  //名称
+        icon: "",  //icon
+        type: "大姐姐",  //类型
+        sp: 1,
+    },
+    {
+        id: "24",  //ID
+        name: "鞠躬",  //名称
+        icon: "",  //icon
+        type: "大姐姐",  //类型
+        sp: 0,
+    },
+    {
+        id: "25",  //ID
+        name: "咻～",  //名称
+        icon: "",  //icon
+        type: "大姐姐",  //类型
+        sp: 0,
+    },
+    {
+        id: "26",  //ID
+        name: "不行不行",  //名称
+        icon: "",  //icon
+        type: "大姐姐",  //类型
+        sp: 0,
+    },
+    {
+        id: "27",  //ID
+        name: "笑哈哈",  //名称
+        icon: "",  //icon
+        type: "大姐姐",  //类型
+        sp: 0,
+    },
+    {
+        id: "28",  //ID
+        name: "交给我",  //名称
+        icon: "",  //icon
+        type: "大姐姐",  //类型
+        sp: 2,
+    },
+    {
+        id: "29",  //ID
+        name: "啊！",  //名称
+        icon: "",  //icon
+        type: "元气",  //类型
+        sp: 1,
+    },
+    {
+        id: "30",  //ID
+        name: "生气",  //名称
+        icon: "",  //icon
+        type: "元气",  //类型
+        sp: 0,
+    },
+    {
+        id: "31",  //ID
+        name: "什么？",  //名称
+        icon: "",  //icon
+        type: "元气",  //类型
+        sp: 0,
+    },
+    {
+        id: "32",  //ID
+        name: "笑眯眯",  //名称
+        icon: "",  //icon
+        type: "元气",  //类型
+        sp: 0,
+    },
+    {
+        id: "33",  //ID
+        name: "打哈欠",  //名称
+        icon: "",  //icon
+        type: "元气",  //类型
+        sp: 0,
+    },
+    {
+        id: "34",  //ID
+        name: "坏笑",  //名称
+        icon: "",  //icon
+        type: "元气",  //类型
+        sp: 2,
+    },
+    {
+        id: "35",  //ID
+        name: "惊！",  //名称
+        icon: "",  //icon
+        type: "成熟",  //类型
+        sp: 0,
+    },
+    {
+        id: "36",  //ID
+        name: "瞪",  //名称
+        icon: "",  //icon
+        type: "成熟",  //类型
+        sp: 0,
+    },
+    {
+        id: "37",  //ID
+        name: "思考",  //名称
+        icon: "",  //icon
+        type: "成熟",  //类型
+        sp: 0,
+    },
+    {
+        id: "38",  //ID
+        name: "叹气",  //名称
+        icon: "",  //icon
+        type: "成熟",  //类型
+        sp: 0,
+    },
+    {
+        id: "39",  //ID
+        name: "喜欢",  //名称
+        icon: "",  //icon
+        type: "成熟",  //类型
+        sp: 2,
+    },
+    {
+        id: "40",  //ID
+        name: "瑟瑟发抖",  //名称
+        icon: "",  //icon
+        type: "普通",  //类型
+        sp: 0,
+    },
+    {
+        id: "41",  //ID
+        name: "兴高采烈",  //名称
+        icon: "",  //icon
+        type: "普通",  //类型
+        sp: 0,
+    },
+    {
+        id: "42",  //ID
+        name: "对吧",  //名称
+        icon: "",  //icon
+        type: "普通",  //类型
+        sp: 0,
+    },
+    {
+        id: "43",  //ID
+        name: "失落",  //名称
+        icon: "",  //icon
+        type: "普通",  //类型
+        sp: 0,
+    },
+    {
+        id: "44",  //ID
+        name: "想象中",  //名称
+        icon: "",  //icon
+        type: "普通",  //类型
+        sp: 2,
+    }
+
+]
